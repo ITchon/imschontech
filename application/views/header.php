7b3dd51fec6376d2f1786c04dad7424b1a2783eb
msuphako <55439852+msuphako@users.noreply.github.com>
2020-12-07 10:20:40 +0700

    calendar

--- application/views/header.php
+++ application/views/header.php
@@ -1,107 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<meta charset="utf-8" />
-		<title><?php echo "TBKK IOT"?></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dashboard &middot; Elephant Template &middot; The fastest way to build Modern Admin APPS for any platform, browser, or device.</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <meta name="description" content="Elephant is an admin template that helps you build modern Admin Applications, professionally fast! Built on top of Bootstrap, it includes a large collection of HTML, CSS and JS components that are simple to use and easy to customize.">
+  
+    <link rel="mask-icon" href="safari-pinned-tab.svg" color="#0288d1">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,700">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/calendar/css/vendor.min.css'; ?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/calendar/css/elephant.min.css'; ?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/calendar/css/application.min.css'; ?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/calendar/css/demo.min.css'; ?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/calendar/css/login-3.min.css';?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/calendar/font-awesome/css/font-awesome.min.css'; ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/calendar/plugins/fullcalendar/fullcalendar.css'; ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/calendar/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css'; ?>">    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js"></script>
 
-		<meta name="description" content="User login page" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-
-        <?php 
-   
-         $css_url = "themes/ace_admin/css/";
-         $asset_url = "themes/ace_admin/";
-         $js_url = "themes/ace_admin/js/";
-        ?>
-		<!-- bootstrap & fontawesome -->
-		<link rel="stylesheet" href="<?php echo base_url() . $css_url; ?>bootstrap.min.css" />
-		<link rel="stylesheet" href="<?php echo base_url() . $asset_url; ?>font-awesome/4.5.0/css/font-awesome.min.css" />
-
-		<!-- page specific plugin styles -->
-		<link rel="stylesheet" href="<?php echo base_url() . $css_url; ?>colorbox.min.css" />
-		<link rel="stylesheet" href="<?php echo base_url() . $css_url; ?>bootstrap-colorpicker.min.css" />
-		<!-- <link rel="stylesheet" href="assets/css/bootstrap-colorpicker.min.css" /> -->
-
-		<link rel="stylesheet" href="<?php echo base_url() . $css_url; ?>dropzone.min.css" />
-
-		<!-- text fonts -->
-		<link rel="stylesheet" href="<?php echo base_url() . $css_url; ?>fonts.googleapis.com.css" />
-
-		<!-- ace styles -->
-		<link rel="stylesheet" href="<?php echo base_url() . $css_url; ?>ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
-
-		<!--[if lte IE 9]>
-			<link rel="stylesheet" href="assets/css/ace-part2.min.css" class="ace-main-stylesheet" />
-		<![endif]-->
-		<link rel="stylesheet" href="<?php echo base_url() . $css_url; ?>ace-skins.min.css" />
-		<link rel="stylesheet" href="<?php echo base_url() . $css_url; ?>ace-rtl.min.css" />
-
-		<!--[if lte IE 9]>
-		  <link rel="stylesheet" href="assets/css/ace-ie.min.css" />
-		<![endif]-->
-
-		<!-- inline styles related to this page -->
-
-		<!-- ace settings handler -->
-		<script src="<?php echo base_url() . $js_url; ?>ace-extra.min.js"></script>
-
-		<!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
-
-		<!--[if lte IE 8]>
-		<script src="assets/js/html5shiv.min.js"></script>
-		<script src="assets/js/respond.min.js"></script>
-		<![endif]-->
-<script src="<?php echo base_url() . $js_url; ?>jquery-2.1.4.min.js"></script>
-
-        <script type="text/javascript">
-			if('ontouchstart' in document.documentElement) document.write("<script src='<?php echo base_url() . $js_url; ?>jquery.mobile.custom.min.js'>"+"<"+"/script>");
-		</script>
-
-        <script src="<?php echo base_url() . $js_url; ?>wizard.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery.validate.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery-additional-methods.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>bootbox.js"></script>
-
-		<script src="<?php echo base_url() . $js_url; ?>spinbox.min.js"></script>
-
-		<script src="<?php echo base_url() . $js_url; ?>jquery.maskedinput.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>select2.min.js"></script>
-
-		<script src="<?php echo base_url() . $js_url; ?>jquery.dataTables.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery.dataTables.bootstrap.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>dataTables.buttons.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>buttons.flash.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>buttons.html5.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>buttons.print.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>buttons.colVis.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>dataTables.select.min.js"></script>
-
-
-		<script src="<?php echo base_url() . $js_url; ?>jquery-ui.custom.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery.ui.touch-punch.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery.easypiechart.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery.sparkline.index.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery.flot.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery.flot.pie.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>jquery.flot.resize.min.js"></script>
-
-
-
-		
-
-		<script src="<?php echo base_url() . $js_url; ?>dropzone.min.js"></script>
-
-		<script src="<?php echo base_url() . $js_url; ?>bootstrap.min.js"></script>
-
-		<!-- page specific plugin scripts -->
-		<script src="<?php echo base_url() . $js_url; ?>jquery.colorbox.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>bootstrap-colorpicker.min.js"></script>
-
-		<!-- ace scripts -->
-		<script src="<?php echo base_url() . $js_url; ?>ace-elements.min.js"></script>
-		<script src="<?php echo base_url() . $js_url; ?>ace.min.js"></script>
-
-
-
-	</head>
+  </head>
