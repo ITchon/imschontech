a95d838e672206dd50e45449cffcfeb315fda3f0
msuphako <55439852+msuphako@users.noreply.github.com>
2021-03-10 14:29:00 +0700

    sal

--- ims.sql
+++ ims.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2021 at 08:00 PM
+-- Generation Time: Mar 10, 2021 at 08:28 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -102,15 +102,15 @@ CREATE TABLE `division` (
 --
 
 INSERT INTO `division` (`dv_id`, `dv_name`) VALUES
-(5, 'แผนกเทคโนโลยีสารสนเทศ'),
-(6, 'แผนกช่างกลโรงงาน'),
-(7, 'แผนกเทคนิคคอมพิวเตอร์'),
-(8, 'แผนกช่างไฟฟ้ากำลัง'),
-(9, 'แผนกช่างเชื่อมโลหะ'),
-(10, 'แผนกระบบขนส่งทางราง'),
-(11, 'แผนกช่างยนต์'),
-(12, 'แผนกสถาปัตยกรรม'),
-(13, 'แผนกช่างก่อสร้าง');
+(5, 'เทคโนโลยีสารสนเทศ'),
+(6, 'ช่างกลโรงงาน'),
+(7, 'เทคนิคคอมพิวเตอร์'),
+(8, 'ช่างไฟฟ้ากำลัง'),
+(9, 'ช่างเชื่อมโลหะ'),
+(10, 'ระบบขนส่งทางราง'),
+(11, 'ช่างยนต์'),
+(12, 'สถาปัตยกรรม'),
+(13, 'ช่างก่อสร้าง');
 
 -- --------------------------------------------------------
 
@@ -135,8 +135,9 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`id`, `title`, `description`, `color`, `start_event`, `end_event`, `std_id`, `teacher_confirm`, `contact_confirm`) VALUES
-(402, '', '', '', '2021-03-11 00:00:00', '0000-00-00 00:00:00', 15, 1, 0),
-(403, '', '', '', '2021-03-10 00:00:00', '0000-00-00 00:00:00', 15, 1, 0);
+(404, '', '', '', '2021-03-15 00:00:00', '0000-00-00 00:00:00', 15, 1, 0),
+(405, '', '', '', '2021-03-10 00:00:00', '0000-00-00 00:00:00', 15, 0, 0),
+(407, 'dasda', '', '', '2021-03-10 06:00:00', '0000-00-00 00:00:00', 15, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -211,7 +212,11 @@ INSERT INTO `group_list` (`glist_id`, `glist_name`) VALUES
 (38, 'test'),
 (43, ''),
 (44, ''),
-(45, 'การแต่งกาย');
+(45, 'การแต่งกาย'),
+(46, 'การแต่งกายรัดกุม'),
+(47, 'การตรงต่อเวลา'),
+(48, 'เข้าใจขั้นตอน'),
+(49, 'ปฏิบัติงานด้วยความรอบคอบและปลอดภัย');
 
 -- --------------------------------------------------------
 
@@ -272,6 +277,9 @@ CREATE TABLE `student_train_detail` (
 `t_id` int(11)
 ,`company_id` int(11)
 ,`company_name` varchar(200)
+,`std_id` int(11)
+,`std_code` varchar(20)
+,`title` varchar(20)
 ,`fname` varchar(50)
 ,`lname` varchar(50)
 ,`dv_id` int(11)
@@ -311,7 +319,8 @@ CREATE TABLE `subject` (
 --
 
 INSERT INTO `subject` (`subject_id`, `subject_name`) VALUES
-(12, 'แบบประเมิน');
+(12, 'แบบประเมินความพึงพอใจ'),
+(13, 'แบบประเมิน');
 
 -- --------------------------------------------------------
 
@@ -330,23 +339,49 @@ CREATE TABLE `subject_grouplist` (
 --
 
 INSERT INTO `subject_grouplist` (`sgl_id`, `subject_id`, `glist_id`) VALUES
-(1, 4, 1),
-(14, 7, 14),
-(16, 7, 16),
-(25, 8, 25),
-(26, 0, 26),
-(27, 0, 27),
-(28, 0, 28),
-(29, 0, 29),
-(30, 0, 30),
-(31, 0, 31),
-(32, 0, 32),
-(33, 0, 33),
-(34, 8, 34),
-(38, 7, 38),
-(43, 0, 43),
-(44, 0, 44),
-(45, 12, 45);
+(46, 12, 46),
+(47, 12, 47),
+(48, 12, 48),
+(49, 12, 49);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject_score`
+--
+
+CREATE TABLE `subject_score` (
+  `ss_id` int(11) NOT NULL,
+  `spv_contact_id` int(11) NOT NULL,
+  `glist_id` int(11) NOT NULL,
+  `score` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subject_score`
+--
+
+INSERT INTO `subject_score` (`ss_id`, `spv_contact_id`, `glist_id`, `score`) VALUES
+(49, 14, 46, 4),
+(50, 14, 47, 3),
+(51, 14, 48, 3),
+(52, 14, 49, 3),
+(53, 15, 46, 4),
+(54, 15, 47, 3),
+(55, 15, 48, 2),
+(56, 15, 49, 3),
+(57, 17, 46, 1),
+(58, 17, 47, 1),
+(59, 17, 48, 1),
+(60, 17, 49, 1),
+(61, 18, 46, 4),
+(62, 18, 47, 3),
+(63, 18, 48, 3),
+(64, 18, 49, 2),
+(65, 19, 46, 3),
+(66, 19, 47, 4),
+(67, 19, 48, 3),
+(68, 19, 49, 4);
 
 -- --------------------------------------------------------
 
@@ -357,10 +392,21 @@ INSERT INTO `subject_grouplist` (`sgl_id`, `subject_id`, `glist_id`) VALUES
 CREATE TABLE `supervision_contact` (
   `spv_contact_id` int(11) NOT NULL,
   `t_id` int(11) NOT NULL,
-  `contact_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL,
-  `approve_date` datetime NOT NULL
+  `approve_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `supervision_contact`
+--
+
+INSERT INTO `supervision_contact` (`spv_contact_id`, `t_id`, `subject_id`, `approve_date`) VALUES
+(14, 11, 12, '2021-03-10'),
+(15, 11, 12, '2021-03-10'),
+(16, 0, 0, '2021-03-10'),
+(17, 11, 12, '2021-03-10'),
+(18, 11, 12, '2021-03-10'),
+(19, 11, 12, '2021-03-10');
 
 -- --------------------------------------------------------
 
@@ -372,7 +418,6 @@ CREATE TABLE `supervision_teacher` (
   `spv_teacher_id` int(11) NOT NULL,
   `t_id` int(11) NOT NULL,
   `teacher_id` int(11) NOT NULL,
-  `subject_id` int(11) NOT NULL,
   `approve_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -426,9 +471,8 @@ CREATE TABLE `train` (
 --
 
 INSERT INTO `train` (`t_id`, `company_id`, `std_id`, `teacher_id`, `contact_id`, `start_date`, `end_date`, `status`, `note`) VALUES
-(11, 16, 15, 9, 3, '2021-04-01', '2021-04-30', '', ''),
-(13, 16, 15, 9, 3, '2021-03-06', '2021-03-31', '', ''),
-(14, 15, 27, 9, 3, '2021-03-06', '2021-03-31', '', '');
+(11, 16, 15, 9, 3, '2022-01-01', '2022-04-08', '', ''),
+(13, 16, 15, 9, 3, '2021-03-06', '2021-03-31', '', '');
 
 -- --------------------------------------------------------
 
@@ -464,13 +508,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `user_id`, `usergroup`, `username`, `password`, `status_login`) VALUES
-(32, 1, 'admin', 'admin', '1234', '1'),
-(33, 15, 'student', '6239010019', '08/12/2543', '1'),
-(34, 10, 'teacher', '1200101896545', '2021-02-26', '1'),
-(46, 1, 'admin', '', '', '1'),
-(47, 11, 'teacher', '1200152354245', '2021-02-24', '1'),
-(48, 9, 'teacher', '1200101964578', '2021-02-24', '1'),
-(49, 27, 'student', '6239045678', '2021-03-17', '0');
+(50, 1, 'admin', 'admin', 'admin', '1'),
+(51, 15, 'student', '6239010019', '08/12/2543', '1'),
+(52, 9, 'teacher', '1200101964578', '2021-02-24', '1');
 
 -- --------------------------------------------------------
 
@@ -479,7 +519,7 @@ INSERT INTO `user` (`id`, `user_id`, `usergroup`, `username`, `password`, `statu
 --
 DROP TABLE IF EXISTS `student_train_detail`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `student_train_detail`  AS SELECT `tn`.`t_id` AS `t_id`, `cp`.`company_id` AS `company_id`, `cp`.`company_name` AS `company_name`, `std`.`fname` AS `fname`, `std`.`lname` AS `lname`, `dv`.`dv_id` AS `dv_id`, `dv`.`dv_name` AS `dv_name`, `c`.`class_id` AS `class_id`, `c`.`class_name` AS `class_name`, `c`.`class_group` AS `class_group`, `ct`.`contact_id` AS `contact_id`, `ct`.`name` AS `name`, `tn`.`start_date` AS `start_date`, `tn`.`end_date` AS `end_date`, `tn`.`status` AS `status`, `th`.`teacher_id` AS `spv_id`, `th`.`title` AS `spv_title`, `th`.`fname` AS `spv_fname`, `th`.`lname` AS `spv_lname`, `c_th`.`teacher_id` AS `cth_id`, `c_th`.`title` AS `cth_title`, `c_th`.`fname` AS `cth_fname`, `c_th`.`lname` AS `cth_lname`, `tn`.`note` AS `note` FROM (((((((`train` `tn` join `company` `cp` on(`cp`.`company_id` = `tn`.`company_id`)) join `teacher` `th` on(`th`.`teacher_id` = `tn`.`teacher_id`)) join `student` `std` on(`std`.`std_id` = `tn`.`std_id`)) join `class` `c` on(`c`.`class_id` = `std`.`class_id`)) join `division` `dv` on(`dv`.`dv_id` = `c`.`dv_id`)) join `teacher` `c_th` on(`c_th`.`teacher_id` = `c`.`teacher_id`)) join `contact` `ct` on(`ct`.`contact_id` = `tn`.`contact_id`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `student_train_detail`  AS SELECT `tn`.`t_id` AS `t_id`, `cp`.`company_id` AS `company_id`, `cp`.`company_name` AS `company_name`, `std`.`std_id` AS `std_id`, `std`.`std_code` AS `std_code`, `std`.`title` AS `title`, `std`.`fname` AS `fname`, `std`.`lname` AS `lname`, `dv`.`dv_id` AS `dv_id`, `dv`.`dv_name` AS `dv_name`, `c`.`class_id` AS `class_id`, `c`.`class_name` AS `class_name`, `c`.`class_group` AS `class_group`, `ct`.`contact_id` AS `contact_id`, `ct`.`name` AS `name`, `tn`.`start_date` AS `start_date`, `tn`.`end_date` AS `end_date`, `tn`.`status` AS `status`, `th`.`teacher_id` AS `spv_id`, `th`.`title` AS `spv_title`, `th`.`fname` AS `spv_fname`, `th`.`lname` AS `spv_lname`, `c_th`.`teacher_id` AS `cth_id`, `c_th`.`title` AS `cth_title`, `c_th`.`fname` AS `cth_fname`, `c_th`.`lname` AS `cth_lname`, `tn`.`note` AS `note` FROM (((((((`train` `tn` join `company` `cp` on(`cp`.`company_id` = `tn`.`company_id`)) join `teacher` `th` on(`th`.`teacher_id` = `tn`.`teacher_id`)) join `student` `std` on(`std`.`std_id` = `tn`.`std_id`)) join `class` `c` on(`c`.`class_id` = `std`.`class_id`)) join `division` `dv` on(`dv`.`dv_id` = `c`.`dv_id`)) join `teacher` `c_th` on(`c_th`.`teacher_id` = `c`.`teacher_id`)) join `contact` `ct` on(`ct`.`contact_id` = `tn`.`contact_id`)) ;
 
 -- --------------------------------------------------------
 
@@ -567,6 +607,18 @@ ALTER TABLE `subject_grouplist`
   ADD PRIMARY KEY (`sgl_id`);
 
 --
+-- Indexes for table `subject_score`
+--
+ALTER TABLE `subject_score`
+  ADD PRIMARY KEY (`ss_id`);
+
+--
+-- Indexes for table `supervision_contact`
+--
+ALTER TABLE `supervision_contact`
+  ADD PRIMARY KEY (`spv_contact_id`);
+
+--
 -- Indexes for table `teacher`
 --
 ALTER TABLE `teacher`
@@ -604,7 +656,7 @@ ALTER TABLE `company`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `division`
@@ -616,7 +668,7 @@ ALTER TABLE `division`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=404;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=408;
 
 --
 -- AUTO_INCREMENT for table `event_img`
@@ -634,7 +686,7 @@ ALTER TABLE `grouplist_list`
 -- AUTO_INCREMENT for table `group_list`
 --
 ALTER TABLE `group_list`
-  MODIFY `glist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `glist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `list`
@@ -652,13 +704,25 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `subject_grouplist`
 --
 ALTER TABLE `subject_grouplist`
-  MODIFY `sgl_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `sgl_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT for table `subject_score`
+--
+ALTER TABLE `subject_score`
+  MODIFY `ss_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+--
+-- AUTO_INCREMENT for table `supervision_contact`
+--
+ALTER TABLE `supervision_contact`
+  MODIFY `spv_contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `teacher`
@@ -676,7 +740,7 @@ ALTER TABLE `train`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
